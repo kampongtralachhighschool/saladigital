c27e1559550b13859e6a2793993880914a0ab429
kampongtralachhighschool <hon.mom.hs@moeys.gov.kh>
2026-04-01 15:30:07 +0700

    Update API_URL to new endpoint

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // ដូរ Link នេះជារៀងរាល់ខែ នៅពេលអ្នកបង្កើត Sheet ថ្មី
-const API_URL = "https://script.google.com/macros/s/AKfycbxU_XQ9PaKLW2U5GzaTKPiIRgUNcSqBQvRjhSzItAu9V2pbrtIJhPczjrpcOshYCk8/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzOAomqmNkCQZRBqMFX2MAffxJRtjG0jwyGhF8hTjtmLiV31cR3-Q5mfFrNu_5BctyS/exec";
 
 
 
